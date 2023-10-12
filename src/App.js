2050ebbe766eda108eb9e40ea67b01d5f47cd0ed
TejaswiniDVekaria <tejaswini.vekaria@fullstacklabs.ca>
2023-10-12 15:05:22 -0400

    JSX

--- src/App.js
+++ src/App.js
@@ -1,44 +1,67 @@
 import React from "react";
 
-// This is our main component
-function App() { 
+function App() {
     
-    //  we gonna return JavaScript xml : This allows us to put html directly into JavaScript.
+    //    before returning we can set variables , create other functions and so on.
+    const book = "Sorathi Baharvatiya"; // This is not the react way of setting up state, in react we have a special hook that known as "useState"
+    const author = "Zaverchand Meghani";
+    const comments = [
+        { id: 1, Comment: "Nice Work" },
+        {id: 2, Comment: "Great staff"},
+        {id: 3, Comment: "Good learning"}
+    ];
+    const loadingComments = false;
+    const showComments = true;
+    if (loadingComments) return <h1>Loading...</h1>
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments : ({comments.length})</h3>
+            <ol>
+                {
+                    comments.map(
+                    (c, i) => (<li key={i}>{c.Comment}</li>)
+                    )
+                }    
+            </ol>
+        </div>
+    )
+    
 
-    // This is looks like html but it is jsx
-    // return (
-        // issue :JSX expressions must have one parent element.
-        // <h1>Welcome to the FB App</h1>
-        // <p>hello</p>
-
-        // To resolve above issue, I take div element as a parent element and write everything inside it only.
-        // <div>
-        // If you don't want to use div then you can use fragment which is <> empty angular brackets.
-        // <>
-        //     <div>
-        //     <h1>Welcome to FB App</h1>
-        //     <p>hello</p>
-        //     </div>
-           
-            /* These 2 things are different in jsx than html */
-            /* 1. I can not use class, it's className here in jsx */
-            /* <div className="jsClass"></div> */
-
-            /* 2.  I can not use for, I can use htmlFor instead */
-            /* <label htmlFor="">Name</label>             */
-        // </>
-        // </div>
-
-    // )
     return (
         <div className="container">
-            <h1>App component</h1>
-            <p>Hey</p>
+            <h1>{book.toUpperCase()}</h1>
+            <p>{author}</p>
+             {/* you can write variable as well as expressions  Curly braces */}
+            {/* {(5 + 5)} : This is the expression */}
+            {/* Comments : {showComments ? 'yes' : false} */}
+            {/* Comments : {showComments ? 'yes' : null} */}
+            {/* Comments : {showComments ? 'yes' : undefined} */}
+            {/* Comments : {showComments ? 'yes' : ""} */}
+            {/* Comments : {showComments && 'yes'} : if you don't want to put 'no' in false condition then you can put null/false/undefined/emptyString which is not showing nothing in the browser and one alternate option could be : you can put && inplace of ? so that you do not have to worry about false condition*/}
+            {/* you can write below div comment function inplace of 'yes' but make sure that should be in (parenthisis) only */}
+
+            {/* Reason : If showComments is falsy (e.g., false, null, undefined, or an empty string), both snippets will not render anything in the browser. This behavior is because React (or JSX in general) does not render null, false, or other falsy values in the output. */}
+
+            {/* <div className="comments">
+                <h3>Comments : ({comments.length})</h3>
+                <ol>
+                    {/* {comments.map((comment, index) => (
+                        <li>{comment.id} : {comment.Comment}</li>
+                    ))} */}
+
+                    
+                    {/* when you create a list using map then you have to provide key to the child prop */}
+                    {/* {comments.map((c, i) => (<li key={i}>{c.Comment}</li>))} */}
+                    {/* ref : li key={c.id} ??? https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key
+                     */}
+                    
+                {/* </ol> */}
+            {/* </div >  */}
+            {/* if you exernalized code for more readability, then keep all the logic in one variable i.e commentBlock */}
+            {showComments&&commentBlock} 
         </div>
     )
 
-    //  this is regular javascript, to write multiple elements would be nightmares with javascript, that is why react is more preferable and it's more readable
-    // return React.createElement('div', { className: 'container' }, React.createElement('h1', {},'Feedback-application'))
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
-// import { createRoot } from 'react-dom';
 import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom' // react library allows us to interact with DOM in the browser
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-// this is the entry point to react in javaScript 
-//  first arg: whatever we wonna insert [which is gonna be our main App Component]
-// second arg: where you want to put it? [Here we put it in the div with the id of 'root']
-
-// ReactDOM.render(<h1>My Feedback App</h1>, document.getElementById('root'))
-
-// Our main App Component displayed here, and any other components we create i.e navigation, feedback-items whatever that's gonna be embedded into main App Component
-// component here either classes(old school way of component creation) or functions (more modern way to create a component : functional component something called hooks).
-
-//StrictMode is offered by react for additional checks and warnings. and we are gonna wrap this using React.StrictMode
 ReactDOM.render(<React.StrictMode><App/></React.StrictMode>, document.getElementById('root'))
-
-// Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17.
-
-
-// const root = document.getElementById('root');
-// const element = <App/>;
-// const rootInstance = createRoot(root);
-// rootInstance.render(element);
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>); 
