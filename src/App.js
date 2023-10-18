1d82798ac05f8f509cac579596a16e8732fbd509
TejaswiniDVekaria <tejaswini.vekaria@fullstacklabs.ca>
2023-10-18 16:05:12 -0400

    react hook : useState

--- src/App.js
+++ src/App.js
@@ -17,3 +17,4 @@ export default App;
 // state is just a data : there are 2 types of states : 
 // 1. component level state : associated with the specific component (No other components need to use that data)
 // 2. App - level/global state : share the state among multiple components.
+// state of react is immutable, you can't change it directly but it has to be set.
--- src/component/FeedbackItem.jsx
+++ src/component/FeedbackItem.jsx
@@ -1,18 +1,50 @@
-// this will include text and ratings
-// write rfce and then press enter key will create function 
-import React from 'react'
+import { useState } from 'react'
 
 function FeedbackItem() {
+    // [what is this function returns : there are 2 pieces]
+    // 1. name of this piece of state : i.e rating/text/anything else
+    // 2. fuction to update this piece of state : setWhatEverNameYouWant
+    // const []=useState(default value) // this useState is a function
+    const [rating, setRating] = useState(5);
+    const [text, setText] = useState("Good :)");
+    const one = () => {
+        setRating(1);
+    }
+    const two = () => {
+        setRating(() => { 
+            // Instead of passing a value I am passing a function here.
+            return 2;
+        });
+    }
+    const three = () => {
+        setRating((prev) => { 
+            // if you want previous value & do some operations then do it this way
+            console.log("previous value is : ",prev)
+            return prev+1;
+        });
+    }
+    const four = () => {
+        setRating(4);
+    }
+    const five = () => {
+        setRating(5);
+    }
   return (
       <div className='feedbackCard'>
           {/* write .rating-display and then press enter key */}
           <div className="ratingDisplay">
-              10
+              {/* name of piece of state */}
+              {rating}
           </div>
           {/* write .text-display and then press enter key */}
           <div className="textDisplay">
-              This is an example of Feedback
+              {text}
           </div>
+          <image src="/Users/tejaswinivekaria/js-react-workspace/src/component/star.png" alt="one" onClick={one} style={{cursor:'pointer'}}>1</image>
+          <button onClick={two}>2</button>
+          <button onClick={three}>3</button>
+          <button onClick={four}>4</button>
+          <button onClick={five}>5</button>
       </div>
   )
 }
