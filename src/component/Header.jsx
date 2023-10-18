448cc751efcca28aceb5b5a8953f7f6c8af6671a
TejaswiniDVekaria <tejaswini.vekaria@fullstacklabs.ca>
2023-10-18 13:08:44 -0400

    set up style through external variable

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'
-function Header({ anyText }) { 
+function Header({ anyText }) {
+  // We can put style in the variable, and pass it in the single curly bracket in the style of header. 
+  const headerStyles = {
+    backgroundColor:'maroon', color: 'green'
+      
+  }
   return (
-    // for inline styling : style="{{double-curly-braces}} "
-    // whatever the properties in the css with hyphen is in camelCase in JavaScript i.e css : background-color == js: backgroundColor
-      <header style={{backgroundColor:'brown', color : 'orange' }}>
+      <header style={headerStyles}>
       <div className="container">
         <h1>{anyText}</h1>
         </div>
