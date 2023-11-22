aa1352eb1c67ba1c6a3d9dfa08e450558448f990
TejaswiniDVekaria <tejaswini.vekaria@fullstacklabs.ca>
2023-11-22 15:18:16 -0500

    check for what should be displayed if the feedbackList

--- src/component/FeedbackList.jsx
+++ src/component/FeedbackList.jsx
@@ -1,5 +1,8 @@
 function FeedbackList({feedbackList }) { 
-    console.log(feedbackList)
+    console.log(feedbackList); 
+    if (!feedbackList || feedbackList.length === 0) { 
+        return "No feedback found yet.";
+    }
     return (
         <div>
         List
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,20 +1,20 @@
 const FeedbackData =[
 // set this array to separate file instead of write it into App.js file.
-    {
-        id: 1,
-        rating: 10,
-        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-        id: 2,
-        rating: 10,
-        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-        id: 3,
-        rating: 10,
-        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    }
+    // {
+    //     id: 1,
+    //     rating: 10,
+    //     text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    // },
+    // {
+    //     id: 2,
+    //     rating: 10,
+    //     text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    // },
+    // {
+    //     id: 3,
+    //     rating: 10,
+    //     text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    // }
 ]
 
 export default FeedbackData
