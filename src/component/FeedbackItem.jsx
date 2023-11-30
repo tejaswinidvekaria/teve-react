556fc338e7792b01a56a9814ced4028b8ca196a1
TejaswiniDVekaria <tejaswini.vekaria@fullstacklabs.ca>
2023-11-30 14:08:30 -0500

    added delete button

--- src/component/FeedbackItem.jsx
+++ src/component/FeedbackItem.jsx
@@ -1,3 +1,5 @@
+import {FaTimes} from 'react-icons/fa'
+// fa means font awesome library
 import CardComponent from './shared/CardComponent'
 
 import PropTypes from 'prop-types'
@@ -6,15 +8,21 @@ function FeedbackItem(
   // we simply passing item in prop of FeedbackItem function.
     { item }
 ) {
+
+  const handleClick=() => console.log(item.id);
    
     return (
         <CardComponent reverse={true}>
             <div className="ratingDisplay">
-                {item.rating}
-            </div>
-            <div className="textDisplay">
+          {item.rating}
+          
+        </div>
+        <button onClick={handleClick} className="close">
+          <FaTimes color='purple'/>
+        </button>
+        <div className="textDisplay">
                 {item.text}
-            </div>
+        </div>
         </CardComponent>
   )
 }
