0411de621dd8092bbdfdadd76de11f388292becd
TejaswiniDVekaria <tejaswini.vekaria@fullstacklabs.ca>
2023-12-05 13:04:22 -0500

    title added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,7 +9,12 @@ function Navbar({title}) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
         <div className='container mx-auto'>
-            Navbar  
+              <div className="flex-none px-2 mx-2">
+                  <FaGithub className='inline pr-2 text-3xl' />
+                  <Link to='/' className='text-lg font-bold align-middle'>
+                      {title}
+                  </Link>
+              </div>
         </div> 
     </nav>
   )
